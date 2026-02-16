923e4c0483da41de48684dac0617519e9f9da9ee
Theshakyhorse <amaximus.sierra@gmail.com>
2026-02-15 16:39:26 -0800

    fourth

--- main.cpp
+++ main.cpp
@@ -5,16 +5,20 @@ using namespace std;
 const int SIZE = 5;
 void enterArrayData(double *);
 void outputArrayData(double *);
+double sumArray(double *);
 
 int main() {
     double *dptr = nullptr;
     dptr = new double[SIZE];
+
     enterArrayData(dptr);
     outputArrayData(dptr);
+    cout << "Sum of values: " << sumArray(dptr);
+
     delete [] dptr;
     return 0;
 }
-
+//populates array with user input
 void enterArrayData(double *Arr){
     double inp;
     cout << "Data entry for the array:" << endl;
@@ -26,10 +30,19 @@ void enterArrayData(double *Arr){
     }
     cout << "Data entry complete." << endl;
 }
-
+//outputs array data in nice format
 void outputArrayData(double *Arr){
     cout << "Outputting array elements: ";
     for(int i = 0; i < SIZE; i++){
         cout << *(Arr + i) << " ";
     }
+    cout << endl;
+}
+//sums array's data and returns it
+double sumArray(double *Arr){
+    double sum;
+    for(int i = 0; i < SIZE; i++){
+        sum += *(Arr + i);
+    }
+    return sum;
 }
