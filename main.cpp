dfff9c422eb85e26b01c1430c62eb6eec0ebdf75
Theshakyhorse <amaximus.sierra@gmail.com>
2026-02-15 16:28:49 -0800

    third

--- main.cpp
+++ main.cpp
@@ -4,11 +4,14 @@ using namespace std;
 
 const int SIZE = 5;
 void enterArrayData(double *);
+void outputArrayData(double *);
 
 int main() {
     double *dptr = nullptr;
     dptr = new double[SIZE];
-    
+    enterArrayData(dptr);
+    outputArrayData(dptr);
+    delete [] dptr;
     return 0;
 }
 
@@ -22,4 +25,11 @@ void enterArrayData(double *Arr){
         cout << endl;
     }
     cout << "Data entry complete." << endl;
+}
+
+void outputArrayData(double *Arr){
+    cout << "Outputting array elements: ";
+    for(int i = 0; i < SIZE; i++){
+        cout << *(Arr + i) << " ";
+    }
 }
